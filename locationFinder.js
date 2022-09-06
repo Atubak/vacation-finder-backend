51abd8cad04784dbf779bc35949398675e98da78
Jona <mr-jona@hotmail.com>
2022-09-06 15:23:06 +0200

    make locationfinder work with one category

--- locationFinder.js
+++ locationFinder.js
@@ -7,10 +7,10 @@ const dir = "./data/formattedData/";
 fs.readdirSync(dir).forEach(function (file) {
   data[file.replace(/\.json$/, "")] = require(dir + file);
 });
-//this console.log lists all the .json files that are imported
+//this console.log lists all the .json files that are imported with their length
 //access a specific .json by specifying a property of data
 //e.g. data.circus
-console.log(Object.keys(data));
+Object.keys(data).forEach((dat) => console.log(dat, data[dat].length));
 
 //function that makes rangeBoxes to find datapoints in
 function rangeBoxer(leastLengthCat) {
@@ -27,8 +27,38 @@ function rangeBoxer(leastLengthCat) {
 }
 
 //function that takes the rangebox array and the datapoints of a single category and adds a datapoint into the object if it falls inside of the rangebox
-function dataPointInserter(allRanges, categoryData) {
-  console.log("hi");
+function dataPointInserter(allRanges, categoryDataName) {
+  //thiscatdata is an array of all datapoints in one category
+  const thisCatData = data[`${categoryDataName}`];
+
+  //if a datapoint is inside, add it to the object and save it for the next round
+  //if a datapoint is NOT inside any range, leave it out
+  //any range that did not receive a new datapoint in this round will also be left out
+
+  const newRangesArray = allRanges.filter((range) => {
+    //for each datapoint check if its inside the given range
+    //if its inside, add it to the range.dataPoints prop
+    // if its not, do nothing
+    thisCatData.forEach((dataP) => {
+      if (
+        dataP.lat <= range.coordinate1.lat &&
+        dataP.lon <= range.coordinate1.lon &&
+        dataP.lat >= range.coordinate2.lat &&
+        dataP.lon >= range.coordinate2.lon
+      ) {
+        //if range.dataPoints does not exist, make it and put in the dataP
+        range.dataPoints
+          ? (range.dataPoints = [...range.dataPoints, dataP])
+          : (range.dataPoints = [dataP]);
+      }
+    });
+
+    //if any range doesnt have the category data name in its range.dataPoints prop array, filter out the range
+    return range.dataPoints.every((datP) => datP.cat === categoryDataName);
+  });
+
+  //when all ranges have been checked, return the new rangesArray
+  return newRangesArray;
 }
 
 //the main function that this app depends on. It would have to be called by the server and its arguments should be each category that was chosen
@@ -43,17 +73,20 @@ function locationFinder(...categories) {
   });
   console.log(orderedCategories);
 
-  //array of all range in which to search for datapoints
+  //array of all ranges in which to search for datapoints
   const rangeBoxArr = rangeBoxer(orderedCategories[0]);
 
-  dataPointInserter(rangeBoxArr, "bingo");
+  //for each category, check whether there is any datapoint within any range
+  //including the original category that was used to create the range in the first place
+  //this way ranges will always have a datapoints array in the first iteration
+  let rangesWithDataPoints = rangeBoxArr;
+  // orderedCategories.forEach((cat) => {
+  rangesWithDataPoints = dataPointInserter(rangeBoxArr, orderedCategories[0]);
+  // });
 
-  //for the next category, check whether there is any datapoint within any range from the last category
-  //if a datapoint is inside, add it to the object and save it for the next round
-  //if a datapoint is NOT inside any range, leave it out
-  //any range that did not receive a new datapoint in this round will also be left out
-
+  console.log(JSON.stringify(rangesWithDataPoints, null, 2));
+  //I THINK IT WORKS
   //repeat the previous block until all categories have been checked
 }
 
-console.log(locationFinder("cinema", "bar", "circus"));
+console.log(locationFinder("restaurant", "circus"));
