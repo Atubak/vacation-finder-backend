a9b63c57478628e21b33a4cd6f7ca22d37b7c10e
Jona <mr-jona@hotmail.com>
2022-09-07 14:47:36 +0200

    refactor post endpoint so it is usable with httpie

--- routers/locations.js
+++ routers/locations.js
@@ -18,10 +18,13 @@ router.get("/example", (req, res, next) => {
 
 //make a post endpoint that recieves an array of categories in the body
 router.post("/", (req, res, next) => {
-  const categories = req.body.categories;
+  //have to split the next line because httpie only accepts one string in the body
+  //when sending httpie post req, make sure to separate each cat with a comma
+  //will fix this when working on frontend
+  const categories = req.body.categories.split(",");
 
   try {
-    res.json(locationFinder(categories));
+    res.json(locationFinder(...categories));
   } catch (e) {
     next(e.message);
   }
