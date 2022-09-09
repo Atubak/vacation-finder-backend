773348f73b4b9a549d71e1bcbbbb6c8cb4d82439
Jona <mr-jona@hotmail.com>
2022-09-09 10:34:14 +0200

    make existing endpoints work more precise with limit of 10 results per search

--- allCategories.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-const allCategories = fs
-  .readdirSync("./data/formattedData")
-  .map((cat) => cat.replace(/\.json$/, ""));
-
-console.log(allCategories);
--- routers/locations.js
+++ routers/locations.js
@@ -1,6 +1,7 @@
 //requires
 const { Router } = require("express");
 const locationFinder = require("../locationFinder");
+const fs = require("fs");
 
 //reading filenames from datafolder
 const allCategories = fs
@@ -21,15 +22,51 @@ router.get("/example", (req, res, next) => {
   }
 });
 
-//make a post endpoint that recieves an array of categories in the body
+//build endpoint that takes all available category files and returns their names
+router.get("/categories", (req, res, next) => {
+  try {
+    res.json(allCategories);
+  } catch (e) {
+    next(e.message);
+  }
+});
+
+//make a post endpoint that recieves an array of categories in the body and returns the ranges that fulfill all the criteria
 router.post("/", (req, res, next) => {
   //have to split the next line because httpie only accepts one string in the body
   //when sending httpie post req, make sure to separate each cat with a comma
   //will fix this when working on frontend
-  const categories = req.body.categories.split(",");
+  console.log(req.body.categories);
+  const categories = req.body.categories;
 
   try {
-    res.json(locationFinder(...categories));
+    const allLocations = locationFinder(...categories);
+
+    //if the result of locationFinder is a string that means there is no valid location with all categories
+    if (typeof allLocations === "string") return res.json(allLocations);
+
+    //if the results are many, take only 10 random results and send it back
+    if (allLocations.length > 10) {
+      let randomLocationsArr = [];
+      let choices = [];
+      for (let i = 0; i < 10; i++) {
+        let newChoiceMade = false;
+        while (!newChoiceMade) {
+          const randomLocationIndex = Math.floor(
+            Math.random() * allLocations.length
+          );
+          if (!choices.includes(randomLocationIndex)) {
+            newChoiceMade = true;
+            choices.push(randomLocationIndex);
+          }
+        }
+      }
+      randomLocationsArr = choices.map((choice) => allLocations[choice]);
+
+      return res.json(randomLocationsArr);
+    }
+
+    res.json(allLocations);
   } catch (e) {
     next(e.message);
   }
